4e513088cd110ae463a8ec0bfca6e7504ceba6f9
Fahad Oni <fahadoni272@gmail.com>
2026-07-22 23:22:56 +0600

    completed Q-11

--- loop.js
+++ loop.js
@@ -92,3 +92,14 @@ for (let i = 1; i <= 50; i++) {
     break;
   }
 }
+
+// Task 11 — Using Continue
+// Write a for loop from 1 to 20 that skips (continue) any number divisible by 4.
+
+for (let i = 1; i <= 20; i++) {
+  // console.log(i);
+  if (i % 4 === 0) {
+    console.log(i);
+    continue;
+  }
+}

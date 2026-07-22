7fefcc62c2b254b54ce47edf1c885f40c52eeffc
Fahad Oni <fahadoni272@gmail.com>
2026-07-22 21:37:47 +0600

    completed Q-3

--- loop.js
+++ loop.js
@@ -1,12 +1,21 @@
 // Task 1 — Print "Hello" Five Times
 
-for (let i = 0; i< 5; i++){
+for (let i = 0; i < 5; i++) {
   console.log(`Hello`);
 }
 
-// Task 2 — Print Numbers 1 to 10 
-let number = 0;
+// Task 2 — Print Numbers 1 to 10
+let number
 
-for (let number = 1; number <= 10; number++){
+for (let number = 1; number <= 10; number++) {
   console.log(number);
+}
+
+// Task 3 — Countdown with While Loop
+
+let num = 10;
+
+while (num > 0) {
+  console.log(num);
+  num--;
 }

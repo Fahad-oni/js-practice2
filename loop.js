5914dca7d2b5958300c2302c83221d509d466cd7
Fahad Oni <fahadoni272@gmail.com>
2026-07-22 21:13:24 +0600

    completed Q-1 & 2

---
+++ loop.js
@@ -0,0 +1,12 @@
+// Task 1 — Print "Hello" Five Times
+
+for (let i = 0; i< 5; i++){
+  console.log(`Hello`);
+}
+
+// Task 2 — Print Numbers 1 to 10 
+let number = 0;
+
+for (let number = 1; number <= 10; number++){
+  console.log(number);
+}

6348540ec09217fe95ebf5788474f2938466936d
Fahad Oni <fahadoni272@gmail.com>
2026-07-22 21:51:03 +0600

    completed Q-4

--- loop.js
+++ loop.js
@@ -1,21 +1,36 @@
 // Task 1 — Print "Hello" Five Times
+// Write a for loop that prints "Hello" 5 times.
 
 for (let i = 0; i < 5; i++) {
   console.log(`Hello`);
 }
 
 // Task 2 — Print Numbers 1 to 10
-let number
+// Write a for loop that prints numbers from 1 to 10.
+
+let number = 0;
 
 for (let number = 1; number <= 10; number++) {
   console.log(number);
 }
 
 // Task 3 — Countdown with While Loop
+// Write a while loop that prints numbers from 10 down to 1.
 
 let num = 10;
 
 while (num > 0) {
   console.log(num);
   num--;
+}
+
+// Task 4 — Repeat a Message
+// Using a while loop, print "Loop চলছে" exactly 7 times.
+
+let message = 'Loop চলছে';
+let i = 1;
+
+while (i <= 7) {
+  console.log('the count is',i, message);
+  i++;
 }

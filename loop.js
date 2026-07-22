2451e719b4ff885d83ff3791c32d7cb9b5be60c6
Fahad Oni <fahadoni272@gmail.com>
2026-07-22 22:49:46 +0600

    completed Q-6

--- loop.js
+++ loop.js
@@ -43,3 +43,12 @@ for (let i = 1; i <= 30; i++) {
     console.log(i);
   }
 }
+
+// Task 6 — Sum of Numbers
+// Write a for loop that calculates the sum of numbers from 1 to 20 and prints the result.
+
+let sum = 0;
+for (let i = 1; i <= 20; i++){
+  sum += i;
+  console.log(`the sum is ${sum}`);
+}
